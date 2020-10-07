32ea431b2d0ddfa97f63c95f65e79d202ff26cce
Benedict Florance <benedictflorance.a@gmail.com>
2020-10-07 22:42:36 +0530

    Debug flatten

--- eval_ssd_network.py
+++ eval_ssd_network.py
@@ -106,6 +106,7 @@ FLAGS = tf.app.flags.FLAGS
 def flatten(x): 
     result = [] 
     for el in x: 
+        print(el)
         if isinstance(el, tuple): 
             result.extend(flatten(el))
         else: 
